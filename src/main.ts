a84d002205ff14cbcdefa1407031d2c6aaf41bba
Allisson <allissonortega04@hotmail.com>
2025-07-25 00:55:06 -0500

    add: swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('terminal-bus example')
+    .setTitle('terminal-bus')
     .setDescription('The terminal-bus API description')
     .setVersion('1.0')
     .addTag('terminal')
